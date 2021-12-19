1f058fb194fa7ea857abdc49f16f90becd3e395c
Sam Abraham Assareymuriyil <samabraham.assareymuriyil@gmail.com>
2021-12-18 20:23:34 -0500

    id

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbtLxUXOwcLqqNAH4OidyntLoKgnjfUPs",
+  authDomain: "photolance-cea62.firebaseapp.com",
+  projectId: "photolance-cea62",
+  storageBucket: "photolance-cea62.appspot.com",
+  messagingSenderId: "382567798911",
+  appId: "1:382567798911:web:f2446abb6008bba996874a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
